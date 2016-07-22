f92d8eb6b9cfac08141948457c57eff6f5a03761
Bryce Jech <bryce@brycejech.com>
2016-07-22 16:58:48 -0500

    Updating pie-slice.js
    Moved some functions around for better readbility.
    Fixed a few of the inconsistent object namings

--- src/js/test.js
+++ src/js/test.js
@@ -1,61 +1,20 @@
 // File for testing pie-slice.js
 
-var r = {
+var test_radio = {
 		lat: 36.113225,
 		lng: -97.058395,
-		deivce_name: 'testAp',
+		device_name: 'testAp',
 		site_name: 'pvn',
 		tx_freq: 2400,
 		band: 2.5,
-		ant_azimuth: 160,
+		ant_azimuth: 180,
 		ant_beamwidth: 90,
-		site_range: 10
+		site_range: 10,
+		ip: '10.1.5.2',
+		tx_chan_width: 20
 };
 
-var testRadio = {
-		lat: 36.113225,
-		lng: -97.058395,
-		deivce_name: 'testAp2',
-		site_name: 'pvn',
-		tx_freq: 2675,
-		band: 5.8,
-		ant_azimuth: 300,
-		ant_beamwidth: 90,
-		site_range: 10
-};
-
-var x = {
-		lat: 36.113225,
-		lng: -97.058395,
-		deivce_name: 'testAp3',
-		site_name: 'pvn',
-		tx_freq: 2450,
-		band: 2.8,
-		ant_azimuth: 200,
-		ant_beamwidth: 90,
-		site_range: 10
-};
-
-var y = {
-		lat: 36.113225,
-		lng: -97.058395,
-		device_name: 'testAp4',
-		site_name: 'pvn',
-		tx_freq: 2075,
-		band: 5.2,
-		ant_azimuth: 60,
-		ant_beamwidth: 90,
-		site_range: 10
-};
-
-pSlice.addRadio(r);
-pSlice.addRadio(testRadio);
-pSlice.addRadio(x);
-pSlice.addRadio(y);
-
-
-
-test_site = {
+var test_site = {
 	'site_name': 	'Test Site 1',
 	'height': 		100,
 	'site_range': 	10,
@@ -63,7 +22,7 @@ test_site = {
 	'type': 		'Rhone 25',
 	'lat': 			36.1132317,
 	'lng': 			-97.0583649
-}
+};
 
-// console.log(pSlice);
 pSlice.add_site(test_site);
+pSlice.add_radio(test_radio);
